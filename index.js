5007f08a55b6f5d22e2b7f437deb1b19f35cff09
FA <fbagmu@gmail.com>
2017-09-28 13:18:41 -0400

    link modified to use piazza

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fetch = require('node-fetch');
 
-fetch('http://api.mywot.com/0.4/public_link_json2?hosts=yahoo.com/&key=6a61298751dcc88830b430677620aadde46cd213')
+fetch('http://api.mywot.com/0.4/public_link_json2?hosts=piazza.com/&key=6a61298751dcc88830b430677620aadde46cd213')
 .then(function(res){
    return res.json();
 }).then(function(json){
