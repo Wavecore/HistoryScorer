0a61bd730fe11c9bc7b12087617d7c73d55da52c
Vincent Liu <liuvincent19915@gmail.com>
2017-10-12 17:24:21 -0400

    worked on sendManyWebsites

--- index.js
+++ index.js
@@ -25,15 +25,22 @@ class WOTRequester{
         this.key = key;
         this.requestTemplate1 = 'http://api.mywot.com/0.4/public_link_json2?hosts=';
         this.requestTemplate2 = '/&key=';
-        this.requestTemplate3 = '&keys=';
     }
-    //TODO format the website so that https://en.google.com/aa/ddd/ will turn as en.google.com
-    formatWebsite(website){
-       // console.log("sadfdf"+website.replace(new RegExp("(.*//)?(www[.])?"),''));
-        return website.replace(new RegExp("(.*//)?(www[.])?"),'').replace(new RegExp("(/.*)"),"");
+    getHostName(url) {
+        var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+        if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+            return match[2];
+        }
+        else {
+            return null;
+        }
     }
     formatWebsiteID(website){
-        return this.formatWebsite(website).replace(new RegExp("[.]",'g'),"_");
+        let formatted = this.getHostName(website);
+        if(formatted != null)
+            return formatted.replace(new RegExp("[.]",'g'),"_");
+        else
+            return null;
     }
     formatResponse(res,website){
         let score = res[website];
@@ -66,54 +73,67 @@ class WOTRequester{
                 return requester.formatResponse(json,website);
             });
     }
-    formatHundredQueries(historyKeys,index){
-        let keysToFormat = new Array();
-        while(keysToFormat.length < 100 && index < historyKeys.length){
-
-        }
-    }
-    addToRequest(reqKeys,res,historyKeys,index){//reqKeys = input keys,res = , historyKeys = output keys, index = index of websites added so far
+    //NOTE: 1 = stopped because reached daily request count
+    //NOTE: 0 = completed normally
+    //NOTE: -1 = Ran into error
+    sendManyWebsites(history,res,index){
+        let keysToFormat = Object.keys(history);
         let requester = this;
-        if(historyKeys.length < 100 && index < reqKeys.length){
-           // console.log(index);
-            let website = requester.formatWebsite(reqKeys[index]);
-            let websiteID = requester.formatWebsiteID(reqKeys[index]);
-           // console.log(reqKeys[index]);
+        if(queriesPerDay > MAXQUERYPERDAY)
+            return res,1;
+        if(keysToFormat.length > index){
+            return this.addToRequest(keysToFormat,res,[],index).then(function(res,webKeys,index){
+                let websites = "";
+                for(let i of webKeys) {
+                    websites += i +"/";
+                }
+                websites = websites.substring(0,websites.length-1);
+                return fetch(requester.requestTemplate1+websites+requester.requestTemplate2+this.key)
+                    .then(function(res){
+                        return res.json();
+                    }).then(function(json){
+                        for(let i of webKeys){
+                            let val = requester.formatResponse(json,i);
+                            val.lastVisitTime =  history[i].lastVisitTime;
+                            val.visits = history[i].visitCount;
+                            res.push({i:val});
+                        }
+                        return requester.sendManyWebsites(history,res,index);
+                    });
+            });
+        }
+        return res,0;
+    }
+    addToRequest(historyKeys,res,webKeys,index){//historyKeys = input keys,res = website information, webKeys = output keys, index = index of websites added so far
+        let requester = this;
+        console.log(historyKeys);
+        if(webKeys.length < 100 && index < historyKeys.length){
+            console.log(index);
+            let websiteID;
+            let website = requester.getHostName(historyKeys[index]);
+            console.log(website);
+            if(website != null)
+                websiteID = requester.formatWebsiteID(historyKeys[index]);
+            else
+                return null;
             index++;
+            console.log("here");
             return database.ref("website/"+websiteID).once("value").then(function(snapshot){
                 if(snapshot.exists() && Date.now()-snapshot.val().lastModified< THIRTYMININMILISEC) {
                     res.push(snapshot.val());
-                    return requester.addToRequest(reqKeys,res,historyKeys,index);
+                    return requester.addToRequest(historyKeys,res,webKeys,index);
                 }
                 else{
-                    historyKeys.push(website);
-                    return requester.addToRequest(reqKeys,res,historyKeys,index);
+                    webKeys.push(website);
+                    return requester.addToRequest(historyKeys,res,webKeys,index);
                 }
             });
         }
         else{
-            console.log("Done");
-            //console.log({res,historyKeys,index});
-            return {res,historyKeys,index};
+            console.log(webKeys);
+            return {res,webKeys,index};
         }
     }
-    /*
-    sendHundredRequests(history,index){
-        
-    }
-
-    sendRequests(history){
-        return new Promise((history,index)=>{
-
-        });
-        return fetch(this.requestTemplate1+websites+this.requestTemplate3+this.key)
-            .then(function(res){
-                return res.json();
-            }).then(function(json){
-                return json;
-            });
-    }
-    */
 }
 
 class History{
@@ -213,6 +233,28 @@ class History{
 }
 var requester = new WOTRequester("6a61298751dcc88830b430677620aadde46cd213");
 var historyReq = new History();
+let tempHistory = {'chrome.google.com':{ id: 11111,
+        visitCount: 2,
+        lastVisitTime: '2017-09-28T15:58:49.045Z',
+        title: 'Chrome Web Store - Extensions',
+        url: 'chrome.google.com' },
+    'search.norton.com':{ id: 22222,
+        visitCount: 183,
+        lastVisitTime: '2017-09-28T15:55:04.884Z',
+        title: 'Safe Search',
+        url: 'search.norton.com' },
+    'amazon.com': { id: 222555,
+        visitCount: 12,
+        lastVisitTime: '2017-09-28T15:57:04.884Z',
+        title: 'Amazon',
+        url: 'amazon.com' },'piazza.com':
+    { id: 33333,
+        visitCount: 30,
+        lastVisitTime: '2017-09-28T15:52:33.483Z',
+        title: 'SWE 432',
+        url: 'piazza.com' }};
+//var temp = requester.addToRequest(Object.keys(tempHistory),{},[],0).then(function(res){console.log(res.webKeys)});
+//var temp = requester.sendManyWebsites(tempHistory,{},0).then(function(res,status){console.log(res);});
 setInterval(()=>{
     queriesPerSec = 0;
     },1000);
