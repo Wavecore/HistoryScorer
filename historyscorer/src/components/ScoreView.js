34c520f6b0087d3a25b82294f2cb298df26fad7b
Vincent Liu <liuvincent19915@gmail.com>
2017-10-30 21:26:24 -0400

    Fixed insecure XMLHttpRequest endpoint issue with ScoreView.js

--- historyscorer/src/components/ScoreView.js
+++ historyscorer/src/components/ScoreView.js
@@ -25,7 +25,7 @@ class ScoreView extends Component {
     }
     componentDidMount() {
         if(Object.keys(this.state.input).length != 0) {
-            fetch("http://localhost:5000/scores", {
+            fetch("https://infinite-peak-34901.herokuapp.com/scores", {
                 headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
                 method: "PUT", body: JSON.stringify({"chrome.google.com":{"visitCount":2,"lastVisitTime":"2017-09-28T15:58:49.045Z","title":"Chrome Web Store - Extensions","url":"chrome.google.com"},"youtube.com":{"visitCount":183,"lastVisitTime":"2017-09-28T15:55:04.884Z","title":"Safe Search Norton","url":"youtube.com"},"amazon.com":{"visitCount":225,"lastVisitTime":"2017-09-28T15:57:04.884Z","title":"Amazon","url":"amazon.com"},"piazza.com":{"visitCount":30,"lastVisitTime":"2017-09-28T15:51:33.483Z","title":"SWE 432","url":"piazza.com"},"demo.com":{"visitCount":17,"lastVisitTime":"2017-09-28T15:52:33.483Z","title":"Demo","url":"demo.com"},"developer.mozilla.org":{"visitCount":17,"lastVisitTime":"2017-09-28T15:52:33.483Z","title":"Demo","url":"developer.mozilla.org"},"regexr.com":{"visitCount":375,"lastVisitTime":"2017-09-28T15:52:33.483Z","title":"Demo","url":"regexr.com"},"piratebay.com":{"visitCount":300,"lastVisitTime":"2017-09-28T15:52:33.483Z","title":"Demo","url":"piratebay.com"},"chia-anime.tv":{"visitCount":300,"lastVisitTime":"2017-09-28T15:52:33.483Z","title":"Demo","url":"chia-anime.tv"}}
                 )
